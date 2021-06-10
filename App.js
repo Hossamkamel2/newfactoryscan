c3ef6d9b01f4f3b4ac9c375c447aba17972cd5b9
Hossamkamel2 <hossamkamel15@gmail.com>
2021-06-10 14:25:29 +0200

    app

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AssetScreen from "./app/screens/AssetScreen";
+import MaterialScreen from "./app/screens/materialScreen";
+import RoomScreen from "./app/screens/RoomScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="asset"
+        component={AssetScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="material"
+        component={MaterialScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Room"
+        component={RoomScreen}
+        options={{
+          title: "Check In/Out material",
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    // </View>
+    <RoomScreen></RoomScreen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
